3e8919d6dc67f0dfbc12beadb99362cb8cf7292d
Jorden <jakereps@users.noreply.github.com>
2016-04-15 15:14:38 -0700

    Redux thunk initial commit

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './components/App';
 import reducer from './reducers';
 
-let store = createStore(reducer);
+let store = createStore(
+    reducer,
+    applyMiddleware(thunk)
+);
 
 render(
     <Provider store={store}>
