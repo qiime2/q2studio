0e8d4e6649b6a48ac7bd87746574119a5ce5fd1a
Jorden <jakereps@users.noreply.github.com>
2016-05-10 12:47:33 -0700

    Add plugin workflow fetching to API

--- qiime_studio/api/v1.py
+++ qiime_studio/api/v1.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify
 from .security import validate_request_authentication
 from qiime.sdk import PluginManager
 
+PLUGIN_MANAGER = PluginManager()
 v1 = Blueprint('v1', __name__)
 v1.before_request(validate_request_authentication)
 
@@ -11,7 +12,18 @@ v1.before_request(validate_request_authentication)
 def root():
     return jsonify(content="!")
 
+
 @v1.route('/plugins', methods=['GET'])
-def plugins():
-    pm = PluginManager()
-    return jsonify(pm.plugins)
+def api_plugins():
+    plugin_list = list(PLUGIN_MANAGER.plugins.keys())
+    return jsonify({"names": plugin_list})
+
+
+@v1.route('/workflows/<plugin_name>', methods=['GET'])
+def api_workflows(plugin_name):
+    plugin = PLUGIN_MANAGER.plugins[plugin_name]
+    workflows_dict = {}
+    for key, value in plugin.workflows.items():
+        workflows_dict[key] = {}
+        workflows_dict[key]['info'] = "Produces: {}".format(list(value.signature.output_artifacts.values()))
+    return jsonify({"workflows": workflows_dict})
