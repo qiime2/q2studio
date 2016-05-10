23814d7bbd6ba4a58510cf8750e8d601b22c7296
Jorden <jakereps@users.noreply.github.com>
2016-05-10 09:28:54 -0700

    Initial interactions with QIIME2

--- qiime_studio/api/v1.py
+++ qiime_studio/api/v1.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify
 
 from .security import validate_request_authentication
 from .cors import add_cors_headers
+from qiime.sdk import PluginManager
 
 v1 = Blueprint('v1', __name__)
 v1.before_request(validate_request_authentication)
@@ -11,3 +12,8 @@ v1.after_request(add_cors_headers)
 @v1.route('/')
 def root():
     return jsonify(content="!")
+
+@v1.route('/plugins', methods=['GET'])
+def plugins():
+    pm = PluginManager()
+    return jsonify(pm.plugins)
