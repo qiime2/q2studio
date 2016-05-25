a65d5c6e71331a4e7c23daac4d5757e75166fa78
Jorden <jakereps@users.noreply.github.com>
2016-05-25 16:10:09 -0700

    Remove hard coded Artifacts

--- app/js/reducers/artifacts.js
+++ app/js/reducers/artifacts.js
@@ -1,15 +1,4 @@
-const initialState = [
-    {
-        name: 'table',
-        uuid: 'f16ca3d0-fe83-4b1e-8eea-7e35db3f6b0f',
-        type: 'FeatureTable[Frequency]'
-    },
-    {
-        name: 'phylogeny',
-        uuid: '908dece5-db23-4562-ad03-876bb5750145',
-        type: 'Phylogeny'
-    }
-];
+const initialState = [];
 
 const artifactsReducer = (state = initialState, action) => {
     switch (action.type) {
