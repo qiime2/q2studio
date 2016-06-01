d0cb04376978665f738a8faaca1dff5f49428f20
Jorden <jakereps@users.noreply.github.com>
2016-06-01 14:33:49 -0700

    Adds Artifact system deletion, confirmation dialog
    Adds DELETE api endpoint for artifacts that handles deletion of the files
    Adds confirmation dialog to UI to prevent accidental deltions.
    Should be changed to custom confirm dialog through React eventually instead of browser alert confirmation style
    Adds Access-Control-Allow-Methods to response headers, as DELETE was being denied without it.

--- qiime_studio/api/cors.py
+++ qiime_studio/api/cors.py
@@ -1,7 +1,9 @@
 
 def add_cors_headers(response):
     response.headers.add('Access-Control-Allow-Headers',
-                         "Authorization, Content-Type, " +
-                         "Content-Length, X-QIIME-Timestamp")
+                         'Authorization, Content-Type, '
+                         'Content-Length, X-QIIME-Timestamp')
     response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET, PUT, POST, DELETE')
     return response
