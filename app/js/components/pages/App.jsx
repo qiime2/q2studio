aada374ded1f559575a0334ca32712da414b5419
Kleptobismol <kestrel.gorlick@gmail.com>
2016-06-24 11:19:59 -0700

    Addresses @ebolyen's comment

--- app/js/components/pages/App.jsx
+++ app/js/components/pages/App.jsx
@@ -14,7 +14,7 @@ const App = ({ connected }) => (
                 <Directory />
             </div>
             <PluginsList />
-            <br></br>
+            <br />
             <JobList />
             <ArtifactsList />
         </div>
