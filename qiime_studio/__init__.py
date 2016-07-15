f6cd13cabac6925cc0575ac2a88a351819650d63
Jai Ram Rideout <jai.rideout@gmail.com>
2016-07-14 18:21:58 -0700

    REL: 0.0.1

--- qiime_studio/__init__.py
+++ qiime_studio/__init__.py
@@ -1,3 +1,13 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+__version__ = "0.0.1"
+
 from .server import start_server
 
 __all__ = ['start_server']
