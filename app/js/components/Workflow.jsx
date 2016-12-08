e9509d5322d2ebf1569e49f929d8215f7bc1ceae
Jorden <jakereps@users.noreply.github.com>
2016-12-07 17:08:39 -0700

    MAINT: Fix responsiveness for smaller screens

--- app/js/components/Workflow.jsx
+++ app/js/components/Workflow.jsx
@@ -12,15 +12,23 @@ const Workflow = ({ flow, onClick, disabled }) => (
     <button type="button" disabled={disabled} className="list-group-item"
         style={{ backgroundColor: disabled ? '#f9f9f9' : '' }} onClick={ onClick }
     >
-        <span className={disabled ? 'col-md-3' : 'col-md-7'}>
+        <span className={disabled ? 'col-md-3 col-xs-4' : 'col-md-7 col-sm-6 col-xs-6'}>
             { flow.name }
         </span>
         {disabled ?
-            <span className="col-md-5">
-                {`Requires: ${flow.requires.join(', ')}`}
+            <span className="col-md-5 col-sm-3 col-xs-3">
+                <span className="visible-md-block">
+                  {`Requires: ${flow.requires.join(', ')}`}
+                </span>
+                <span
+                    className="glyphicon glyphicon-warning-sign pull-right
+                               visible-sm-block visible-xs-block"
+                    title={`Requires: ${flow.requires.join(', ')}`}
+                    aria-hidden="true"
+                ></span>
             </span> : null
         }
-        <span className={disabled ? 'col-md-4' : 'col-md-5'}>
+        <span className={disabled ? 'col-md-4 col-xs-5' : 'col-md-5 col-sm-6 col-xs-6'}>
             Produces: { flow.outputs.map(({ type }) => type).join(', ') }
         </span>
     </button>
