bf07e83ac049496ccc34595f11dc543971301f6e
Max von Hippel <maxvonhippel1996@gmail.com>
2017-06-05 13:55:31 -0700

    added alphabetical sorting of types and formats lists per review suggestion

--- app/js/reducers/supertypes.js
+++ app/js/reducers/supertypes.js
@@ -50,7 +50,7 @@ const reducer = (state = initialState, action) => {
     case 'IMPORTABLE_TYPES': {
         const newState = {
             ...state,
-            importableTypes: action.importableTypesList.map(
+            importableTypes: action.importableTypesList.sort().map(
                 item => ({ value: item, label: item })
             )
         };
@@ -59,7 +59,7 @@ const reducer = (state = initialState, action) => {
     case 'IMPORTABLE_FORMATS': {
         const newState = {
             ...state,
-            importableFormats: action.importableFormatsList.map(
+            importableFormats: action.importableFormatsList.sort().map(
                 item => ({ value: item, label: item })
             )
         };
