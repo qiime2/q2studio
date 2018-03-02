48c3073430d8362537771cdd51ebe5894b125252
Matthew Dillon <matthewrdillon@gmail.com>
2018-03-02 10:26:37 -0700

    VER: 2018.4.0.dev0 (#156)

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ with open("README.md") as fh:
 
 setup(
     name='q2studio',
-    version='2018.2.0',
+    version='2018.4.0.dev0',
     license='BSD-3-Clause',
     url='https://qiime2.org',
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['click', 'flask', 'gevent', 'qiime2 == 2018.2.*']
+    install_requires=['click', 'flask', 'gevent', 'qiime2 == 2018.4.*']
 )
